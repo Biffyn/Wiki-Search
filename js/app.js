93c3eec9e43cd0a04775bbbf5fdd85326278dc79
biffyn <nuravenweb@gmail.com>
2016-09-28 22:37:21 +0200

    Removed css lib, begin work on api call

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
+    var searchQuery;
+    var api_url = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&prop=extracts&exlimit=max&explaintext&exintro&exsentences=2&gsrsearch=";
 
+    function search(){
+        searchQuery = "hello"; //encodeURIComponent($("#search").val().trim());
+        url =   api_url + searchQuery + "&callback=?";
+
+        $.getJSON(url, function(res){
+            console.log(res);
+
+        }); // end getJSON
+
+    } // end search()
+         search();
 });     //end doc.ready
