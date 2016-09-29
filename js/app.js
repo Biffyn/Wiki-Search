643ffa87dbf0a0e1d22d52e0ba97bb45c4eb0aa6
biffyn <nuravenweb@gmail.com>
2016-09-29 19:42:25 +0200

    Added ajax call

--- js/app.js
+++ js/app.js
@@ -1,16 +1,31 @@
 $(document).ready(function() {
-    var searchQuery;
-    var api_url = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&prop=extracts&exlimit=max&explaintext&exintro&exsentences=2&gsrsearch=";
+    var searchQuery = $("#search").val();
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + searchQuery + "&callback=?";
 
-    function search(){
-        searchQuery = "hello"; //encodeURIComponent($("#search").val().trim());
-        url =   api_url + searchQuery + "&callback=?";
+    function search() {
+        $("#searchIcon").on("click", function() {
+                searchQuery = encodeURIComponent($("#search").val());
+                console.log(searchQuery);
+                $.ajax({
+                        type: "GET",
+                        url: url,
+                        async: false,
+                        dataType: "json",
+                        data: {
+                            "action": "opensearch",
+                            "format": "json",
+                            "limit": 10,
+                            "search": searchQuery
+                        },
+                        success: function(data) {
+                            console.log(data);
+                        },
+                        error: function(err) {
+                            console.log(err);
+                        }
+                });
 
-        $.getJSON(url, function(res){
-            console.log(res);
-
-        }); // end getJSON
-
-    } // end search()
-         search();
-});     //end doc.ready
+        }); // end search.on()click
+} // end search()
+search();
+}); //end doc.ready
